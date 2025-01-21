d22adbfd6b40a4e2b0a623e94f3c5b94e09bbb77
r0bertleberger <raphael.casanova@orange.fr>
2025-01-21 09:29:05 +0100

    interface web marche

--- projet/clean.sh
+++ projet/clean.sh
@@ -4,4 +4,3 @@
 rm /home/pi/Git/hailo-rpi5-examples/projet/buffer-videos/output.mp4 /home/pi/Git/hailo-rpi5-examples/projet/buffer-videos/output-bis.mp4 /home/pi/Git/hailo-rpi5-examples/projet/resultat.txt 
 
 rm /home/pi/Git/hailo-rpi5-examples/projet/resultat-bis.txt
-touch /home/pi/Git/hailo-rpi5-examples/projet/resultat-bis.txt
